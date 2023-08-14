3dc134d72dcd961827247751da78dabbb0ea9474
Aiden Lignieres <46545209+aidenlignieres@users.noreply.github.com>
2023-08-14 18:07:07 +1000

    Task 1 steps 6-8 complete

---
+++ app/src/main/java/com/example/guesstheceleb/ImageManager.java
@@ -0,0 +1,14 @@
+package com.example.guesstheceleb;
+
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+
+public class ImageManager {
+    private String assetPath;
+    private String[] imageNames;
+    private AssetManager assetManager;
+
+    ImageManager(AssetManager assetManager, String assetPath) {}
+
+    Bitmap get(int i) {return null; }
+}
--- app/src/main/java/com/example/guesstheceleb/MainActivity.java
+++ app/src/main/java/com/example/guesstheceleb/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.guesstheceleb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +24,13 @@ public class MainActivity extends AppCompatActivity {
         try {
             String[] names = manager.list("celebs");
             Log.d("celebs",Arrays.toString(names));
+
+            ImageView imageView = findViewById(R.id.image);
+
+            InputStream stream = manager.open("celebs/" + names[0]);
+            Bitmap bitmap = BitmapFactory.decodeStream(stream);
+
+            imageView.setImageBitmap(bitmap);
         } catch (IOException e) {
             Log.d("TAG","Failed to get names");
         }
