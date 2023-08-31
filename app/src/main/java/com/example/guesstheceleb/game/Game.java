1f9a8664b920ec7653c8c497ca09ba776b79994a
Aiden Lignieres <46545209+aidenlignieres@users.noreply.github.com>
2023-08-31 10:27:28 +1000

    Task 1 Step 7

--- app/src/main/java/com/example/guesstheceleb/game/Game.java
+++ app/src/main/java/com/example/guesstheceleb/game/Game.java
@@ -1,4 +1,34 @@
 package com.example.guesstheceleb.game;
 
 public class Game {
+    private Question[] questions;
+    private int currentQuestionIndex;
+    private int score;
+
+    public Game(Question[] questions) {
+        this.questions = questions;
+        this.currentQuestionIndex = 0;
+        this.score = 0;
+    }
+
+    public Question next() {
+        if (currentQuestionIndex < questions.length) {
+            return questions[currentQuestionIndex++];
+        }
+        return null;
+    }
+
+    public boolean isGameOver() {
+        return currentQuestionIndex >= questions.length;
+    }
+
+    public void updateScore(boolean isCorrect) {
+        if (isCorrect) {
+            score++;
+        }
+    }
+
+    public String getScore() {
+        return "Score: " + score + "/" + questions.length;
+    }
 }
