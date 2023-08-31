b99390e650b49e4e64f8b8d7054c6c644790882e
Aiden Lignieres <46545209+aidenlignieres@users.noreply.github.com>
2023-08-31 10:09:58 +1000

    Task 1 Step 1 & 2

--- app/src/main/java/com/example/guesstheceleb/game/CelebrityManager.java
+++ app/src/main/java/com/example/guesstheceleb/game/CelebrityManager.java
@@ -1,4 +1,4 @@
-package com.example.guesstheceleb;
+package com.example.guesstheceleb.game;
 
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
@@ -7,12 +7,12 @@ import android.graphics.BitmapFactory;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class ImageManager {
+public class CelebrityManager {
     private String assetPath;
     private String[] imageNames;
     private AssetManager assetManager;
 
-    ImageManager(AssetManager assetManager, String assetPath) {
+    public CelebrityManager(AssetManager assetManager, String assetPath) {
         this.assetPath = assetPath;
         this.assetManager = assetManager;
         try {
@@ -22,7 +22,7 @@ public class ImageManager {
         }
     }
 
-    Bitmap get(int i) {
+    public Bitmap get(int i) {
         if (imageNames != null && i >= 0 && i < imageNames.length) {
             InputStream stream = null;
             try {
@@ -42,5 +42,23 @@ public class ImageManager {
             }
         }
         return null;
+    }
+
+    public String getName(int i) {
+        if (imageNames != null && i >= 0 && i < imageNames.length) {
+            String imageName = imageNames[i];
+            int dotIndex = imageName.lastIndexOf('.');
+            if (dotIndex >= 0) {
+                return imageName.substring(0, dotIndex);
+            }
+        }
+        return null;
+    }
+
+    public int count() {
+        if (imageNames != null) {
+            return imageNames.length;
+        }
+        return 0;
     }
 }
