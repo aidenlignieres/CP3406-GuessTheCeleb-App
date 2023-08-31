607a99d26c439575ff6c4cdb8be9e6c31339c398
Aiden Lignieres <46545209+aidenlignieres@users.noreply.github.com>
2023-08-31 12:02:14 +1000

    Task 2 Step 9

--- app/src/main/java/com/example/guesstheceleb/StatusFragment.java
+++ app/src/main/java/com/example/guesstheceleb/StatusFragment.java
@@ -3,11 +3,13 @@ package com.example.guesstheceleb;
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class StatusFragment extends Fragment {
 
@@ -16,6 +18,9 @@ public class StatusFragment extends Fragment {
 
     private String mParam1;
     private String mParam2;
+
+    private TextView message;
+    private TextView score;
 
     public StatusFragment() {
         // Required empty public constructor
@@ -40,9 +45,22 @@ public class StatusFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_status, container, false);
+        View view = inflater.inflate(R.layout.fragment_status, container, false);
+
+        message = view.findViewById(R.id.message);
+        score = view.findViewById(R.id.score);
+
+        return view;
+    }
+
+    public void setMessage(String text) {
+        message.setText(text);
+    }
+
+    public void setScore(String text) {
+        message.setText(text);
     }
 
     private StateListener listener;
